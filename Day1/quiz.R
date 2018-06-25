158802249d1b0a48fecfc9ff19136d8b9fc72142
huutrinh68 <trinhsp89@gmail.com>
2018-06-25 10:05:26 -0700

    add Day1

--- Day1/quiz.R
+++ Day1/quiz.R
@@ -3,9 +3,9 @@ head(iris.data)
 dim(iris.data)
 str(iris.data)
 levels(iris.data$Species)
-iris.data[,]
-iris.data[,3]
-iris.data[,1:3]
-iris.data[,-5]
-iris.data[,c(3,4)]
-iris.data[,-c(3,4)]
+iris.data[,] # all data
+iris.data[,3] # only colum 3
+iris.data[,1:3] # colum from 1 to 3
+iris.data[,-5] # except colum 5
+iris.data[,c(3,4)] # colum 3 and colum 4
+iris.data[,-c(3,4)] # except colum 3 and colum 4
